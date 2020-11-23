dd04cc190ecc3dcc970be29144fadc87a5f62c97
轩 <wyx@MacBook-Pro.local>
2020-11-23 14:35:34 +0800

    1123

--- code/static/js/encryption.js
+++ code/static/js/encryption.js
@@ -3,8 +3,17 @@ const encrypt = new JSEncrypt();
 const decrypt = new JSEncrypt();
 
 const RsaEncrypt = {
-
-}
+    msgEncrypt : function (msg,key){
+        encrypt.setPublicKey(key);
+        let encrypted = encrypt.encrypt(msg);
+        return encrypted;
+    },
+    msgDecrypt : function (encMsg,key){
+        decrypt.setPrivateKey(key);
+        let decrypted = decrypt.decrypt(encMsg);
+        return decrypt;
+    }
+};
 const AesEncrypt = {
-    
-}
+
+};
